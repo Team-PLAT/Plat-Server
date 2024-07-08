2a8b82f571e4bb017f96993db3c3e76b072c53c2
KyungsooLee <jmtkd9196@gmail.com>
2024-07-08 17:23:32 +0900

    Init spring boot project

---
+++ src/main/java/com/cabin/plat/PlatApplication.java
@@ -0,0 +1,13 @@
+package com.cabin.plat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cabin/plat/PlatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabin.plat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
