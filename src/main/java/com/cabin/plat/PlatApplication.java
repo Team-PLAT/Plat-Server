914ff4942f5f007df94fb444f9e32756f07d06f3
KyungsooLee <jmtkd9196@gmail.com>
2024-07-11 18:00:46 +0900

    chore: #4 baseResponse 테스트 코드 추가

--- src/main/java/com/cabin/plat/PlatApplication.java
+++ src/main/java/com/cabin/plat/PlatApplication.java
@@ -2,10 +2,10 @@ package com.cabin.plat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class PlatApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PlatApplication.class, args);
 	}
