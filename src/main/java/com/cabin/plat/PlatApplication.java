1e4acb9fb8595283039378a2ec6fe78c9f4a409e
KyungsooLee <jmtkd9196@gmail.com>
2024-07-28 03:34:01 +0900

    feat: #17 SpringSecurity 임시 적용 해제

--- src/main/java/com/cabin/plat/PlatApplication.java
+++ src/main/java/com/cabin/plat/PlatApplication.java
@@ -3,8 +3,9 @@ package com.cabin.plat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class PlatApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PlatApplication.class, args);
