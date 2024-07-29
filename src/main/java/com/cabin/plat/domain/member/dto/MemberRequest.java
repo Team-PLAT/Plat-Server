953937631d1037b8c63f015d9098e9001e8aefa5
KyungsooLee <jmtkd9196@gmail.com>
2024-07-29 23:36:33 +0900

    feat: #17 MemberSignIn Request DTO 구현

--- src/main/java/com/cabin/plat/domain/member/dto/MemberRequest.java
+++ src/main/java/com/cabin/plat/domain/member/dto/MemberRequest.java
@@ -1,4 +1,23 @@
 package com.cabin.plat.domain.member.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 public class MemberRequest {
+
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class MemberSignIn {
+        private String encryptedUserIdentifier;
+        private String fullName;
+        private String email;
+    }
+
+
+
 }
