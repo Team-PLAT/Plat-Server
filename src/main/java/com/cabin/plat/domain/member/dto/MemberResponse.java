b60506e3997c7955afca4bcbb1b7f46e4ff0145b
KyungsooLee <jmtkd9196@gmail.com>
2024-07-29 23:36:58 +0900

    feat: #17 MemberSignIn Response DTO 구현

--- src/main/java/com/cabin/plat/domain/member/dto/MemberResponse.java
+++ src/main/java/com/cabin/plat/domain/member/dto/MemberResponse.java
@@ -1,4 +1,20 @@
 package com.cabin.plat.domain.member.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 public class MemberResponse {
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class MemberSignIn {
+        private Long memberId;
+        private String accessToken;
+        private String refreshToken;
+        private Boolean isServicedMember;
+    }
 }
