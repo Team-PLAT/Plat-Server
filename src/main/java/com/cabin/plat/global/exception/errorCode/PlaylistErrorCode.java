6cbae504203800d451744ef7df663cfec4071f6c
dgh06175 <dgh06175@gmail.com>
2024-09-21 15:35:43 +0900

    feat: #44 플레이리스트 에러 코드 작성

--- src/main/java/com/cabin/plat/global/exception/errorCode/PlaylistErrorCode.java
+++ src/main/java/com/cabin/plat/global/exception/errorCode/PlaylistErrorCode.java
@@ -1,4 +1,33 @@
 package com.cabin.plat.global.exception.errorCode;
 
-public enum PlaylistErrorCode {
+import com.cabin.plat.global.exception.ErrorCode;
+import com.cabin.plat.global.exception.ErrorCodeInterface;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+public enum PlaylistErrorCode implements ErrorCodeInterface{
+    PLAYLIST_NOT_FOUND("PLAYLIST001", "Playlist가 존재하지 않습니다.", HttpStatus.NOT_FOUND),
+    PLAYLIST_DELETE_FORBIDDEN("PLAYLIST002", "이 플레이리스트를 삭제할 권한이 없습니다.", HttpStatus.FORBIDDEN),
+    PLAYLIST_TRACK_NOT_FOUND("PLAYLIST003", "플레이리스트에 해당 트랙이 존재하지 않습니다.", HttpStatus.NOT_FOUND),
+    PLAYLIST_TRACK_DELETE_FORBIDDEN("PLAYLIST004", "이 플레이리스트의 트랙을 삭제할 권한이 없습니다.", HttpStatus.FORBIDDEN),
+    ;
+
+    private final String code;
+    private final String message;
+    private final HttpStatus httpStatus;
+
+    PlaylistErrorCode(String code, String message, HttpStatus httpStatus) {
+        this.code = code;
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+
+    public ErrorCode getErrorCode() {
+        return ErrorCode.builder()
+                .code(code)
+                .message(message)
+                .httpStatus(httpStatus)
+                .build();
+    }
 }
